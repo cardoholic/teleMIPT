97c5beb12200389e647059cf4c7c06345a84b225
tusaeff <tusaeff@yandex.ru>
2017-06-06 01:29:53 +0300

    кастом клава

--- bot.py
+++ bot.py
@@ -7,9 +7,8 @@ from datetime import datetime, date
 from flask import Flask, request
 import statistic
 import os
-# import psycopg2
-# from urllib.parse import urlparse
 from database import db, Prepod, Stats, server
+
 
 bot = telebot.TeleBot("349791719:AAGz3KaZsc3OPuj1D4rtxIVWtVZr9azAqG0")
 url = 'https://api.telegram.org/bot349791719:AAGz3KaZsc3OPuj1D4rtxIVWtVZr9azAqG0/'
@@ -43,14 +42,17 @@ def telemipt(message):
                     if (IS_LOGGING):
                         log(message, answer)
                 else:
+                    markup = telebot.types.ReplyKeyboardMarkup(row_width=1)
                     for item in result:
                         #чтобы ссылка красиво выглядела
-                        message_url = url + 'sendMessage' + '?chat_id=' + str(message.chat.id) + \
-                                      '&text=<a href="' + item['href'] + '">' + item['name'] + '</a>&parse_mode=HTML'
-                        requests.get(message_url)
-                        answer = item['name']
+                        # message_url = url + 'sendMessage' + '?chat_id=' + str(message.chat.id) + \
+                        #               '&text=<a href="' + item['href'] + '">' + item['name'] + '</a>&parse_mode=HTML'
+                        # requests.get(message_url)
+                        markup.add(telebot.types.KeyboardButton(item['name']))
+                        # answer = item['name']
                         if (IS_LOGGING):
                             log(message, answer)
+                    bot.sendMessage(message.chat.id, "Выберите преподавателя:", reply_markup=markup)
             elif (type(result) == dict):
                 for key in result:
                     if (type(result[key]) == list):
